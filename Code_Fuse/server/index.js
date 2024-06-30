4efadca04fcc5a704a0403f5831a6e7627f2e2c3
Shubham Kolkar <shubhamkolkar03@gmail.com>
2024-06-30 14:53:05 +0530

    conected to sever

--- Code_Fuse/server/index.js
+++ Code_Fuse/server/index.js
@@ -1,4 +1,12 @@
 const  express = require('express')
 const app = express();
 
+
+const {Server}= require("socket.io")
+const http = require('http');
+
+env.config();
+const server = http.createServer(app);
+const io= new Server(server);
+
 const port = process.env.PORT || 5000;   
